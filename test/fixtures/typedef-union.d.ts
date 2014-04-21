c05d726f4bcd50dbd75505a779eb4c0a6c652c80
teppeis <teppeis@gmail.com>
2014-04-22 08:39:58 +0900

    Ref #2 @typedef union type

---
+++ test/fixtures/typedef-union.d.ts
@@ -0,0 +1,7 @@
+declare module goog.array {
+
+    /**
+     * @typedef {Array|NodeList|Arguments|{length: number}}
+     */
+    export interface ArrayLike {}
+}
---
+++ test/fixtures/typedef-union.js
@@ -0,0 +1,6 @@
+goog.provide('goog.array');
+
+/**
+ * @typedef {Array|NodeList|Arguments|{length: number}}
+ */
+goog.array.ArrayLike;
