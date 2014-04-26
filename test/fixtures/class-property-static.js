2e83b0eb95e2b619e95ca405e5f60c0b4c95963d
teppeis <teppeis@gmail.com>
2014-04-26 11:05:24 +0900

    #16 Class properties

---
+++ test/fixtures/class-property-static.js
@@ -0,0 +1,13 @@
+goog.provide('goog.dom');
+
+/**
+ * @param {Document} document
+ * @constructor
+ */
+goog.dom.DomHelper = function(document) {
+};
+
+/**
+ * @type {string}
+ */
+goog.dom.DomHelper.foo = '';
