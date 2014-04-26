e1ff28d1f74c1470add6b015e4876050b8596610
teppeis <teppeis@gmail.com>
2014-04-26 13:11:48 +0900

    Support for @private classes

---
+++ test/fixtures/class-private.js
@@ -0,0 +1,22 @@
+goog.provide('goog.dom');
+
+/**
+ * @param {Document} document
+ * @constructor
+ * @private
+ */
+goog.dom.DomHelper = function(document) {
+};
+
+/**
+ * @param {string} str
+ * @return {number}
+ */
+goog.dom.DomHelper.prototype.foo = function(str) {
+    return 1;
+};
+
+/**
+ * @type {string}
+ */
+goog.dom.DomHelper.prototype.bar = '';
