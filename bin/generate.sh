d13b4f19d6a41d014723f9b107dac1c76a626650
teppeis <teppeis@gmail.com>
2014-04-27 00:22:22 +0900

    Enable third_party dir

--- bin/generate.sh
+++ bin/generate.sh
@@ -2,6 +2,6 @@
 
 BASEDIR=$(cd $(dirname $0)/..; pwd;)
 
-find $BASEDIR/closure-library/closure/goog -type f -name '*.js'|grep -v _test|sort|
+find $BASEDIR/closure-library -type f -name '*.js'|grep -v _test|sort|
     grep -v /demos/|
     xargs $BASEDIR/bin/closurets.js
