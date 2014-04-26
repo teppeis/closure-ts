519169358a14ebe9aa5696757d08b8b4a420dd12
teppeis <teppeis@gmail.com>
2014-04-27 08:06:39 +0900

    Don't generate empty .d.ts files

--- bin/generate.sh
+++ bin/generate.sh
@@ -2,6 +2,8 @@
 
 BASEDIR=$(cd $(dirname $0)/..; pwd;)
 
+find closure-library -type f -name '*.d.ts'|xargs rm
 find $BASEDIR/closure-library -type f -name '*.js'|grep -v _test|sort|
     grep -v /demos/|
     xargs $BASEDIR/bin/closurets.js
+$BASEDIR/bin/generate-alldts.sh
