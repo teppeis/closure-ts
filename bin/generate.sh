309db0e65964f1b44420a99edf9e11a39aba724f
teppeis <teppeis@gmail.com>
2014-04-26 22:54:58 +0900

    Enable goog/datasource/jsondatasource.js

--- bin/generate.sh
+++ bin/generate.sh
@@ -4,7 +4,6 @@ BASEDIR=$(cd $(dirname $0)/..; pwd;)
 
 find $BASEDIR/closure-library/closure/goog -type f -name '*.js'|grep -v _test|sort|
     grep -v /demos/|
-    grep -v goog/datasource/jsondatasource.js|
     grep -v goog/iter/iter.js|
     grep -v goog/labs/promise/promise.js|
     grep -v goog/testing/async/mockcontrol.js|
