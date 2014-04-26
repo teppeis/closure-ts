60e59b292eb313ba751807f9108753ea906ac8f4
teppeis <teppeis@gmail.com>
2014-04-27 00:27:07 +0900

    Add generate-alldts.sh

---
+++ bin/generate-alldts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+BASEDIR=$(cd $(dirname $0)/..; pwd;)
+CLOSURE_DIR=$BASEDIR/closure-library
+
+find $CLOSURE_DIR -type f -name '*.d.ts'|sort|
+    sed -e 's/.*\/closure-library\//\/\/\/ <reference path="/g' -e 's/$/" \/>/g' > $CLOSURE_DIR/all.d.ts
