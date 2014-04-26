60c90d0b290a100f0b839eb1dcfcfe99780b3727
teppeis <teppeis@gmail.com>
2014-04-27 00:13:41 +0900

    Enable goog/labs/promise/promise.d.ts

--- bin/generate.sh
+++ bin/generate.sh
@@ -4,5 +4,4 @@ BASEDIR=$(cd $(dirname $0)/..; pwd;)
 
 find $BASEDIR/closure-library/closure/goog -type f -name '*.js'|grep -v _test|sort|
     grep -v /demos/|
-    grep -v goog/labs/promise/promise.js|
     xargs $BASEDIR/bin/closurets.js
