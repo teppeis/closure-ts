2bd2d0494145acef4f08ce6e897c29e29f9d9cd6
teppeis <teppeis@gmail.com>
2014-04-26 17:01:54 +0900

    Add generate.sh

---
+++ bin/generate.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+BASEDIR=$(cd $(dirname $0)/..; pwd;)
+
+find $BASEDIR/closure-library/closure/goog -type f -name '*.js'|grep -v _test|sort|
+    grep -v /demos/|
+    grep -v goog/datasource/jsondatasource.js|
+    grep -v goog/editor/plugins/linkbubble.js|
+    grep -v goog/iter/iter.js|
+    grep -v goog/labs/promise/promise.js|
+    grep -v goog/net/multiiframeloadmonitor.js|
+    grep -v goog/promise/promise.js|
+    grep -v goog/reflect/reflect.js|
+    grep -v goog/soy/renderer.js|
+    grep -v goog/soy/soy.js|
+    grep -v goog/string/stringformat.js|
+    grep -v goog/structs/stringset.js|
+    grep -v goog/testing/asserts.js|
+    grep -v goog/testing/async/mockcontrol.js|
+    grep -v goog/testing/testcase.js|
+    grep -v goog/ui/containerrenderer.js|
+    grep -v goog/ui/editor/bubble.js|
+    grep -v goog/ui/imagelessbuttonrenderer.js|
+    grep -v goog/ui/paletterenderer.js|
+    grep -v goog/ui/sliderbase.js|
+    grep -v goog/ui/style/app/buttonrenderer.js|
+    xargs $BASEDIR/bin/closurets.js
