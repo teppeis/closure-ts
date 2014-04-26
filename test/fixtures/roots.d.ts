9261e55c71485e81fbd7c838e9f6ee2c3bb987da
teppeis <teppeis@gmail.com>
2014-04-27 08:26:24 +0900

    Fix #26 limit root modules

---
+++ test/fixtures/roots.d.ts
@@ -0,0 +1,31 @@
+declare module goog {
+
+    /**
+     * @param {number} num
+     */
+    export function foo(num: number): void;
+}
+
+declare module proto2 {
+
+    /**
+     * @param {number} num
+     */
+    export function foo(num: number): void;
+}
+
+declare module osapi {
+
+    /**
+     * @param {number} num
+     */
+    export function foo(num: number): void;
+}
+
+declare module svgpan {
+
+    /**
+     * @param {number} num
+     */
+    export function foo(num: number): void;
+}
---
+++ test/fixtures/roots.js
@@ -0,0 +1,39 @@
+// Export
+
+/**
+ * @param {number} num
+ */
+goog.foo = function(num) {};
+
+/**
+ * @param {number} num
+ */
+proto2.foo = function(num) {};
+
+/**
+ * @param {number} num
+ */
+osapi.foo = function(num) {};
+
+/**
+ * @param {number} num
+ */
+svgpan.foo = function(num) {};
+
+// Ignore
+
+/**
+ * @param {number} num
+ */
+global.foo = function(num) {};
+
+/**
+ * @param {number} num
+ */
+self.foo = function(num) {};
+
+
+/**
+ * @param {number} num
+ */
+window.foo = function(num) {};
