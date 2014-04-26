42000851e28242099a23fa14ca3eee8fbe893bcb
teppeis <teppeis@gmail.com>
2014-04-26 17:31:27 +0900

    #23 Update closure-library

--- bin/generate.sh
+++ bin/generate.sh
@@ -8,11 +8,8 @@ find $BASEDIR/closure-library/closure/goog -type f -name '*.js'|grep -v _test|so
     grep -v goog/iter/iter.js|
     grep -v goog/labs/promise/promise.js|
     grep -v goog/reflect/reflect.js|
-    grep -v goog/soy/renderer.js|
-    grep -v goog/soy/soy.js|
     grep -v goog/string/stringformat.js|
     grep -v goog/testing/async/mockcontrol.js|
     grep -v goog/ui/imagelessbuttonrenderer.js|
-    grep -v goog/ui/paletterenderer.js|
     grep -v goog/ui/style/app/buttonrenderer.js|
     xargs $BASEDIR/bin/closurets.js
