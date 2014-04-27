d9f4e3ee166b1322a3dfa6004d94ea8e64044681
teppeis <teppeis@gmail.com>
2014-04-27 18:02:30 +0900

    generate.sh compiles all .d.ts and outputs error.log

--- bin/generate.sh
+++ bin/generate.sh
@@ -7,3 +7,6 @@ find $BASEDIR/closure-library -type f -name '*.js'|grep -v _test|sort|
     grep -v /demos/|
     xargs $BASEDIR/bin/closurets.js
 $BASEDIR/bin/generate-alldts.sh
+tsc $BASEDIR/test/all.ts 2>&1 | sed -e 's/\/.*\/closure-ts\///g' > $BASEDIR/error.log
+ack 'TS\d+' $BASEDIR/error.log -o|sort|uniq -c|sort -r > $BASEDIR/errorcode.log
+ack '^closure[^\(]*' $BASEDIR/error.log -o|sort|uniq -c|sort -r > $BASEDIR/ranking.log
