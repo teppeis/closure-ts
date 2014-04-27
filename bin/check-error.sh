7ad7d48e5db6e474aee7d62d90d765f19008d5d8
teppeis <teppeis@gmail.com>
2014-04-28 03:04:41 +0900

    Update check-error.sh

--- bin/check-error.sh
+++ bin/check-error.sh
@@ -2,6 +2,4 @@
 
 BASEDIR=$(cd $(dirname $0)/..; pwd;)
 
-tsc $BASEDIR/test/all.ts 2>&1 | sed -e 's/\/.*\/closure-ts\///g' > $BASEDIR/error.log
-ack 'TS\d+' $BASEDIR/error.log -o|sort|uniq -c|sort -r > $BASEDIR/errorcode.log
-ack '^closure[^\(]*' $BASEDIR/error.log -o|sort|uniq -c|sort -r > $BASEDIR/ranking.log
+tsc $BASEDIR/test/all.ts 2>&1 | sed -e 's/\/.*\/closure-ts\///g' |tee $BASEDIR/error.log
