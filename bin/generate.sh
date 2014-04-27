9ca83fd335befe4a772321059d5989d32c38c685
teppeis <teppeis@gmail.com>
2014-04-27 21:53:05 +0900

    Ignore goog.testing and goog.result

--- bin/generate.sh
+++ bin/generate.sh
@@ -5,6 +5,9 @@ BASEDIR=$(cd $(dirname $0)/..; pwd;)
 find closure-library -type f -name '*.d.ts'|grep -v '/externs/'|xargs rm
 find $BASEDIR/closure-library -type f -name '*.js'|grep -v _test|sort|
     grep -v /demos/|
+    grep -v /goog/result/|
+    grep -v /goog/testing/|
+    grep -v /goog/net/mockiframeio.js|
     xargs $BASEDIR/bin/closurets.js
 $BASEDIR/bin/generate-alldts.sh
 $BASEDIR/bin/check-error.sh
