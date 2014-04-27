0047de55c0408850219060d4d9ead33aecd7b340
teppeis <teppeis@gmail.com>
2014-04-27 18:38:01 +0900

    Expose private typedefs

---
+++ test/fixtures/typedef-private.d.ts
@@ -0,0 +1,10 @@
+declare module goog.array {
+
+    /**
+     * Export private @typedef as interface.
+     * @private
+     * @typedef {Array|NodeList|Arguments|{length: number}}
+     */
+    export interface ArrayLike {
+    }
+}
---
+++ test/fixtures/typedef-private.js
@@ -0,0 +1,8 @@
+goog.provide('goog.array');
+
+/**
+ * Export private @typedef as interface.
+ * @private
+ * @typedef {Array|NodeList|Arguments|{length: number}}
+ */
+goog.array.ArrayLike;
