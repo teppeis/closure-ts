cd702a6e44b1f59cc8e4161caa8f23910666e96b
teppeis <teppeis@gmail.com>
2014-04-27 15:19:26 +0900

    Add externs/w3c_indexeddb.d.ts

--- bin/generate.sh
+++ bin/generate.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(cd $(dirname $0)/..; pwd;)
 
-find closure-library -type f -name '*.d.ts'|xargs rm
+find closure-library -type f -name '*.d.ts'|grep -v '/externs/'|xargs rm
 find $BASEDIR/closure-library -type f -name '*.js'|grep -v _test|sort|
     grep -v /demos/|
     xargs $BASEDIR/bin/closurets.js
