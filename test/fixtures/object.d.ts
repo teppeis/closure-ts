9ae6014688aea37bab09d2d9926cd64e9b984eaf
teppeis <teppeis@gmail.com>
2015-02-26 23:03:35 +0900

    feat(object): support generics of Object like `Object<string>` or `Object<string, number>`

--- test/fixtures/object.d.ts
+++ test/fixtures/object.d.ts
@@ -5,5 +5,15 @@ declare module goog.functions {
      * @return {Object.<T>}
      * @template T
      */
-    function toObject<T>(item: T): Object;
+    function toObject<T>(item: T): {[index: string]: T};
+
+    /**
+     * @return {Object<number, boolean>}
+     */
+    function numboolean(): {[index: number]: boolean};
+
+    /**
+     * @return {Object<Date, boolean>}
+     */
+    function dateShouldBeConvertedToString(): {[index: string]: boolean};
 }
--- test/fixtures/object.js
+++ test/fixtures/object.js
@@ -1,10 +1,22 @@
-goog.provide('goog.functions');
-
 /**
  * @param {T} item
  * @return {Object.<T>}
  * @template T
  */
 goog.functions.toObject = function(item) {
+  return null;
+};
+
+/**
+ * @return {Object<number, boolean>}
+ */
+goog.functions.numboolean = function() {
+  return null;
+};
+
+/**
+ * @return {Object<Date, boolean>}
+ */
+goog.functions.dateShouldBeConvertedToString = function() {
   return null;
 };
