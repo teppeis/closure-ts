1f1dc5b51243972860dee83459d4df1f5b3c7624
teppeis <teppeis@gmail.com>
2015-03-01 16:52:50 +0900

    fix(module): goog.require will not return non-initiated module

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-BASEDIR=$(cd $(dirname $0)/..; pwd;)
+cd $(dirname $0)/..
+BASEDIR=.
 
 find $BASEDIR/closure-library.d.ts -type f -name '*.d.ts'|grep -v '/externs/'|xargs rm
-find $BASEDIR/closure-library -type f -name '*.js'|grep -v _test|sort|
+find $BASEDIR/closure-library -type f -name '*.js'|sort|
     grep -v /demos/|
+    grep -v /testdata|
+    grep -v /testing/|
+    grep -v '_test.js$'|
+    grep -v 'tester.js$'|
+    grep -v '_perf.js$'|
     grep -v /goog/result/|
-    grep -v /goog/testing/|
     grep -v /goog/net/mockiframeio.js|
-    grep -v tester.js|
+    grep -v /goog/proto2/test.pb.js|
+    diff --unchanged-line-format='' --new-line-format='' - ./bin/ignore.txt|
     xargs $BASEDIR/bin/closurets.js
 cp $BASEDIR/builtin.d.ts $BASEDIR/closure-library.d.ts/externs/
 $BASEDIR/bin/generate-alldts.sh
