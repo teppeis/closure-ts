620a311f30c313de38ea633d3ea5a0983a627b75
teppeis <teppeis@gmail.com>
2018-11-16 23:48:45 +0900

    build: update cli-color, eslint and prettier

--- test/parse.js
+++ test/parse.js
@@ -6,23 +6,25 @@ const generator = require('../lib/generator');
 
 describe('Generator', () => {
   const files = fs.readdirSync(`${__dirname}/fixtures`);
-  return files.filter(file => /\.js$/.test(file)).forEach(file =>
-    it(file, () => {
-      const code = fs.readFileSync(`${__dirname}/fixtures/${file}`, 'utf8');
-      const actual = generator.generate(code);
-      const expected = fs.readFileSync(
-        `${__dirname}/fixtures/${file.replace(/\.js$/, '.d.ts')}`,
-        'utf8'
-      );
-      if (actual !== expected) {
-        diff.diffChars(expected, actual).forEach(part => {
-          let value;
-          value = part.value;
-          value = part.added ? `[+${value}]` : part.removed ? `[-${value}]` : value;
-          return process.stderr.write(value);
-        });
-        throw new Error('Different result');
-      }
-    })
-  );
+  return files
+    .filter(file => /\.js$/.test(file))
+    .forEach(file =>
+      it(file, () => {
+        const code = fs.readFileSync(`${__dirname}/fixtures/${file}`, 'utf8');
+        const actual = generator.generate(code);
+        const expected = fs.readFileSync(
+          `${__dirname}/fixtures/${file.replace(/\.js$/, '.d.ts')}`,
+          'utf8'
+        );
+        if (actual !== expected) {
+          diff.diffChars(expected, actual).forEach(part => {
+            let value;
+            value = part.value;
+            value = part.added ? `[+${value}]` : part.removed ? `[-${value}]` : value;
+            return process.stderr.write(value);
+          });
+          throw new Error('Different result');
+        }
+      })
+    );
 });
