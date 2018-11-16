9fddc83c2dd89a262a95c612fa477ddd94a2ab21
teppeis <teppeis@gmail.com>
2018-11-16 23:43:27 +0900

    fix: use npx and fix typescript version in check-error

--- bin/check-error.sh
+++ bin/check-error.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(cd $(dirname $0)/..; pwd;)
 
-tsc $BASEDIR/test/all.ts 2>&1 | sed -e 's/\/.*\/closure-ts\///g' |tee $BASEDIR/error.log
+npx typescript@1.4 $BASEDIR/test/all.ts 2>&1 | sed -e 's/\/.*\/closure-ts\///g' | tee $BASEDIR/error.log
