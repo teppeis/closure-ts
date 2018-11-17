56f53c6282d84614baf10ba3ce76d83085ec7c27
teppeis <teppeis@gmail.com>
2018-11-17 20:55:33 +0900

    fix: add IArrayLike and HTMLIsIndexElement to builtin.d.ts

--- builtin.d.ts
+++ builtin.d.ts
@@ -1,3 +1,14 @@
 declare module goog {
     type globalEventTarget = EventTarget;
 }
+
+interface IArrayLike<VALUE> {
+    length: number;
+    [key: number]: VALUE;
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLIsIndexElement
+interface HTMLIsIndexElement extends HTMLElement {
+    from: HTMLFormElement;
+    prompt: string;
+}
