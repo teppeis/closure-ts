a9265844f26c905ed2ffd40a68a978bf15b9b67d
teppeis <teppeis@gmail.com>
2018-11-17 21:39:40 +0900

    fix: replace NodeList<T> to NodeListOf<T>

---
+++ test/fixtures/nodelist.d.ts
@@ -0,0 +1,12 @@
+declare module goog.functions {
+
+    /**
+     * @return {NodeList<Element>} returns list of Element.
+     */
+    function foo(): NodeListOf<Element>;
+
+    /**
+     * @return {NodeList} returns NodeList.
+     */
+    function bar(): NodeList;
+}
