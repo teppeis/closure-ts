0da92d8638534ccbc9f3a636531c646445cf7132
teppeis <teppeis@gmail.com>
2018-11-17 21:42:24 +0900

    fix: replace HTMLMenuItemElement and HTMLIsIndexElement to HTMLElement

--- builtin.d.ts
+++ builtin.d.ts
@@ -6,9 +6,3 @@ interface IArrayLike<VALUE> {
     length: number;
     [key: number]: VALUE;
 }
-
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLIsIndexElement
-interface HTMLIsIndexElement extends HTMLElement {
-    from: HTMLFormElement;
-    prompt: string;
-}
