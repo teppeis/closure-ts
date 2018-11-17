822dc32016d60e0dc6d3f3f1f726d289c354ae42
teppeis <teppeis@gmail.com>
2018-11-17 22:27:01 +0900

    feat: support @record

---
+++ test/fixtures/record.js
@@ -0,0 +1,20 @@
+/**
+ * @record
+ */
+goog.functions.Foo = function() {};
+
+/**
+ * @type {number}
+ */
+goog.functions.Foo.prototype.foo;
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+goog.functions.Foo.prototype.bar;
+
+/**
+ * No @param and @return.
+ */
+goog.functions.Foo.prototype.baz;
