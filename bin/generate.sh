bb6ef39a2c1cabef0391767fd06fd420932f5800
teppeis <teppeis@gmail.com>
2018-11-18 13:15:17 +0900

    fix: remove /testing/, goog.result and goog.net.MockIFrameIo from ignore.txt

--- bin/generate.sh
+++ bin/generate.sh
@@ -6,14 +6,10 @@ BASEDIR=.
 find $BASEDIR/closure-library.d.ts -type f -name '*.d.ts'|grep -v '/externs/'|xargs rm
 find $BASEDIR/closure-library/closure/goog $BASEDIR/closure-library/third_party/closure/goog -type f -name '*.js'|sort|
     grep -v /demos/|
-    grep -v /testdata|
-    grep -v /testing/|
+    grep -v /testdata/|
     grep -v '_test.js$'|
     grep -v 'tester.js$'|
     grep -v '_perf.js$'|
-    grep -v /goog/result/|
-    grep -v /goog/net/mockiframeio.js|
-    grep -v /goog/proto2/test.pb.js|
     diff --unchanged-line-format='' --new-line-format='' - ./bin/ignore.txt|
     xargs $BASEDIR/bin/closurets.js
 cp $BASEDIR/builtin.d.ts $BASEDIR/closure-library.d.ts/externs/
