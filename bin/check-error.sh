02d2e58e95424a806c9c6de9b58887b400067fd3
teppeis <teppeis@gmail.com>
2018-11-18 14:15:34 +0900

    fix: remove useless npx log from error.log

--- bin/check-error.sh
+++ bin/check-error.sh
@@ -4,4 +4,5 @@ BASEDIR=$(cd "$(dirname "$0")/.."; pwd;) || exit 1
 
 npx typescript@3.1 --noEmit --lib DOM,ES2018,DOM.Iterable "$BASEDIR/test/all.ts" 2>&1 | \
     sed -e 's/\/.*\/closure-ts\///g' | \
+    sed -e '/^npx: /d' | \
     tee "$BASEDIR/misc/error.log"
