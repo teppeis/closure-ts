b47b1b8f90266a7fd97b6dfcf8d3f8aea486dbd2
teppeis <teppeis@gmail.com>
2018-11-18 13:00:26 +0900

    build: mkdir misc for notes

--- bin/check-error.sh
+++ bin/check-error.sh
@@ -4,4 +4,4 @@ BASEDIR=$(cd "$(dirname "$0")/.."; pwd;) || exit 1
 
 npx typescript@3.1 --noEmit --lib DOM,ES2018,DOM.Iterable "$BASEDIR/test/all.ts" 2>&1 | \
     sed -e 's/\/.*\/closure-ts\///g' | \
-    tee "$BASEDIR/error.log"
+    tee "$BASEDIR/misc/error.log"
